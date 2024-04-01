dc666f13b93afab8573a06ef1eb20e2494ccadec
zarx011 <129439378+zarx011@users.noreply.github.com>
2024-04-01 02:27:36 -0400

    have *some* kinda working suite labels

--- blackjack.rb
+++ blackjack.rb
@@ -10,10 +10,16 @@
 			# CARDS
 	# duck typing https://www.codingninjas.com/studio/library/type-checking-and-duck-typing-in-ruby
 
+
+	# Setting up window & background
 	set title: "Blackjack", background: 'gray', resizable: true
 	Image.new("blackjack-table.png", x: 80, y:80, width:450, height: 350, z:1)
+
+	# Blank card to display shuffled cards
 	Image.new("blankcard.png", x: 255, y:320, width: 100, height: 150, z:5)
 	Image.new("blankcard.png", x: 280, y:20, width: 50, height: 75, z:5)
+
+	
 	song = Music.new('musicloop.mp3') #https://www.ruby2d.com/learn/audio/
 	song.loop = true
 	song.volume = 50
@@ -23,49 +29,50 @@
 	play = Sound.new('playcard.mp3')
 	shuffle.play
 
-	class Game
-		attr_reader :text_player_card, :text_dealer_card
-	  
-		def initialize(player)
-		  @deck = Deck.new
-		  @player = User.new(player)
-		  @dealer = Dealer.new
-		  @text_player_card = Text.new("", x: 255, y: 320, size: 20, color: 'white')
-		  @text_dealer_card = Text.new("", x: 280, y: 20, size: 20, color: 'white')
-		end
-	  
-		public  # Make subsequent methods public
-	  
-		def player_turn(&block)
-		  yield if block_given?
-		end
-	  
-		def dealer_turn(&block)
-		  yield if block_given?
-		end
-	  
-		def deal_cards_initially
-		  player_card = @deck.draw
-		  dealer_card = @deck.draw
-	  
-		  update_card_text(@text_player_card, player_card)
-		  update_card_text(@text_dealer_card, dealer_card)
-		end
-	  
-		private
-	  
-		def update_card_text(text_object, card)
-		  suit_icons = {
-			"Hearts" => "♥",
-			"Diamonds" => "♦",
-			"Clubs" => "♣",
-			"Spades" => "♠"
-		  }
-		  text_object.text = "Card: #{card.rank} #{suit_icons[card.suit]}"
-		end
-	  end
 
-	  
+	# Create text objects for displaying card information
+	text_player_card = Text.new("", x: 255, y: 320, size: 20, color: 'white')
+	text_dealer_card = Text.new("", x: 280, y: 20, size: 20, color: 'white')
+
+	# Game class
+	class Game
+		def initialize(player, text_player_card, text_dealer_card)
+			@deck = Deck.new
+			@player = User.new(player)
+			@dealer = Dealer.new
+			@text_player_card = text_player_card
+			@text_dealer_card = text_dealer_card
+		end
+	
+		public
+		def player_turn(&block)
+			yield if block_given?
+		end
+	
+		def dealer_turn(&block)
+			yield if block_given?
+		end
+	
+		def deal_cards_initially
+			player_card = @deck.draw
+			dealer_card = @deck.draw
+		
+			update_card_text(@text_player_card, player_card)
+			update_card_text(@text_dealer_card, dealer_card)
+		end
+	
+		private
+		def update_card_text(text_object, card)
+			suit_icons = {
+				"Hearts" => "♥",
+				"Diamonds" => "♦",
+				"Clubs" => "♣",
+				"Spades" => "♠"
+			}
+			text_object.text = "Card: #{card.rank} #{suit_icons[card.suit]}"
+		end
+	end # end game
+
 
 		#blocking
 		#https://medium.com/rubycademy/the-yield-keyword-603a850b8921
@@ -146,17 +153,24 @@
 
 	end
 
-	game = Game.new("Player") # new game
-	game.deal_cards_initially # do initial dealing
+
+	# Initialize and deal cards for the game
+	game = Game.new("Player", text_player_card, text_dealer_card)
+	game.deal_cards_initially
+
+	# Show the window
+	show
+
+#	game = Game.new("Player") # new game
+	#game.deal_cards_initially # do initial dealing
 
 	#blocking
-	game.player_turn do
+#	game.player_turn do
 		#
-	end
-	game.dealer_turn do
-		#
-	end
+#	game.dealer_turn do
+#		#
+#	end
 
 
 	#show window
-	show
+	#show
