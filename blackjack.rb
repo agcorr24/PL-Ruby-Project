59be70e7fa66ca3a5e620b4437ca7c7bfb77e444
zarx011 <129439378+zarx011@users.noreply.github.com>
2024-04-07 14:26:58 -0400

    need to fix some stuff but working on stand method

--- blackjack.rb
+++ blackjack.rb
@@ -25,7 +25,7 @@ dealer_card2 = Image.new("blankcard.png", x: 310, y: 20, width: 50, height: 75, 
 player_card1 = Image.new("blankcard.png", x: 190, y: 320, width: 100, height: 150, z: 5)
 player_card2 = Image.new("blankcard.png", x: 310, y: 320, width: 100, height: 150, z: 5)
 
-	
+    
 # creating sound effects
 song = Music.new('musicloop.mp3') #https://www.ruby2d.com/learn/audio/
 song.loop = true
@@ -46,236 +46,241 @@ text_player_card2 = Text.new("", x: 420, y: 400, size: 20, color: 'white')
 
 # for card values
 text_player_total = Text.new("", x: 30, y: 360, size: 20, color: 'white', z: 10)
-text_dealer_total = Text.new("", x: 380, y: 50, size: 20, color: 'white', z: 10)
+text_dealer_total = Text.new("", x: 470, y: 50, size: 20, color: 'white', z: 10)
+
+# initialize dealer card 2 text with empty string
+text_dealer_card2.remove
 
 # Game class
 class Game
-	# intiialize game, player, dealer, and text boxes for card values
-	def initialize(player, text_player_card1, text_player_card2, text_dealer_card1, text_player_total, text_dealer_total)
-	  @deck = Deck.new
-	  @player = User.new(player)
-	  @dealer = Dealer.new
-	  @text_player_card1 = text_player_card1
-	  @text_player_card2 = text_player_card2
-	  @text_dealer_card1 = text_dealer_card1
-	  @text_player_total = text_player_total
-	  @text_dealer_total = text_dealer_total
-	end
+    # intiialize game, player, dealer, and text boxes for card values
+    def initialize(player, text_player_card1, text_player_card2, text_dealer_card1, text_dealer_card2, text_player_total, text_dealer_total)
+      @deck = Deck.new
+      @player = User.new(player)
+      @dealer = Dealer.new
+      @text_player_card1 = text_player_card1
+      @text_player_card2 = text_player_card2
+      @text_dealer_card1 = text_dealer_card1
+	  @text_dealer_card2 = text_dealer_card2
+      @text_player_total = text_player_total
+      @text_dealer_total = text_dealer_total
+    end
   
-	# only shuffles at beginning - initial deal
-	def deal_cards_initially
-	  2.times do
-		player_card = @deck.draw
-		dealer_card = @deck.draw
+    # only shuffles at beginning - initial deal
+    def deal_cards_initially
+      2.times do
+        player_card = @deck.draw
+        dealer_card = @deck.draw
   
-		@player.add_card(player_card)
-		# initial deal for player
-		update_card_text(@text_player_card1, player_card) if @player.player_hand.size == 1
-		update_card_text(@text_player_card2, player_card) if @player.player_hand.size == 2
+        @player.add_card(player_card)
+        # initial deal for player
+        update_card_text(@text_player_card1, player_card) if @player.player_hand.size == 1
+        update_card_text(@text_player_card2, player_card) if @player.player_hand.size == 2
   
-		@dealer.add_card(dealer_card)
-		# initial deal for dealer
-		update_card_text(@text_dealer_card1, dealer_card) if @dealer.dealer_hand.size == 1
-	  end
-	  update_player_total
-	  #update_dealer_total
-	end
+        @dealer.add_card(dealer_card)
+        # initial deal for dealer
+        update_card_text(@text_dealer_card1, dealer_card) if @dealer.dealer_hand.size == 1
+		update_card_text(@text_dealer_card2, dealer_card) if @dealer.dealer_hand.size == 1
+
+      end
+      update_player_total
+      #update_dealer_total
+    end
   
-	# if player wants to add another card to their sum
-	def hit
-		new_card = @deck.draw
-		@player.add_card(new_card) # Fixed reference to @player
-		update_card_text(@text_player_card2, new_card) # Update the text for the new card
+    # if player wants to add another card to their sum
+    def hit
+        new_card = @deck.draw
+        @player.add_card(new_card) # Fixed reference to @player
+        update_card_text(@text_player_card2, new_card) # Update the text for the new card
 
-		update_player_total
-	end
+        update_player_total
+    end
   
-	# if player wants to keep current sum
-	def stand
-	  # Implement dealer logic here, if needed
-	  dealer_turn
-	  update_dealer_total
-
+    # if player wants to keep current sum
+    def stand
+      # Implement dealer logic here, if needed
+      dealer_turn
+      update_dealer_total
+	  update_card_text(@text_dealer_card2, @dealer.dealer_hand.last) if @dealer.dealer_hand.size == 2
 	end
 
-	private 
-	
-	# to display on text for player and dealer - card values and suites
-	def update_card_text(text_object, card)
-		suit_icons = {
-			"Hearts" => "♥",
-			"Diamonds" => "♦",
-			"Clubs" => "♣",
-			"Spades" => "♠"
-		  }
-		  card_text = "#{card.rank} #{suit_icons[card.suit]}"
-		  text_object.text = card_text
-		end
+    private 
+    
+    # to display on text for player and dealer - card values and suites
+    def update_card_text(text_object, card)
+        suit_icons = {
+            "Hearts" => "♥",
+            "Diamonds" => "♦",
+            "Clubs" => "♣",
+            "Spades" => "♠"
+          }
+          card_text = "#{card.rank} #{suit_icons[card.suit]}"
+          text_object.text = card_text
+        end
 
-		def update_player_total
-			total_value = @player.total
-			@text_player_total.text = "Total: #{total_value} "
-		end
+        def update_player_total
+            total_value = @player.total
+            @text_player_total.text = "Total: #{total_value} "
+        end
 
-		def update_dealer_total
-			total_value = @dealer.total
-			@text_dealer_total.text = "Total: #{total_value} "
-		end
-	end 
+        def update_dealer_total
+            total_value = @dealer.total
+            @text_dealer_total.text = "Total: #{total_value} "
+        end
+    end 
 
-	#blocking
-	#https://medium.com/rubycademy/the-yield-keyword-603a850b8921
-	def player_turn(&block)
-		yield if block_given?
-	end
+    #blocking
+    #https://medium.com/rubycademy/the-yield-keyword-603a850b8921
+    def player_turn(&block)
+        yield if block_given?
+    end
 
-	def dealer_turn(&block)
-		#yield if block_given?
-		while @dealer.total < 17
-			dealer_hit
-			yield if block_given? # Yield to a block if provided
-		  end
-	end
+    def dealer_turn(&block)
+        #yield if block_given?
+        while @dealer.total < 17
+            dealer_hit
+            yield if block_given? # Yield to a block if provided
+          end
+    end
 
-	def dealer_hit
-		new_card = @deck.draw
-		@dealer.add_card(new_card)
-  		update_card_text(@text_dealer_card2, new_card)
-  	end
+    def dealer_hit
+        new_card = @deck.draw
+        @dealer.add_card(new_card)
+    end
 
-	# regular expression and mapping
-	def check_for_blackjack(player)
-		#
-	end
-#end game
+    # regular expression and mapping
+    def check_for_blackjack(player)
+        #
+    end
+#end # end game
 
 
 class Card
-	# attribute features in ruby that creates getter methods so we can easily change them:
-	# https://medium.com/@rossabaker/what-is-the-purpose-of-attr-accessor-in-ruby-3bf3f423f573#:~:text=to%20help%20out.-,attr_reader,color%20%23%20%3C%2D%2D%20Getter%20methods
-	attr_reader :rank, :suit, :value
-	# initialize card rank(s) and suite(s)
-	def initialize (rank, suit)
-		@rank = rank
-		@suit = suit
-		@value = calculate_value
-	end
+    # attribute features in ruby that creates getter methods so we can easily change them:
+    # https://medium.com/@rossabaker/what-is-the-purpose-of-attr-accessor-in-ruby-3bf3f423f573#:~:text=to%20help%20out.-,attr_reader,color%20%23%20%3C%2D%2D%20Getter%20methods
+    attr_reader :rank, :suit, :value
+    # initialize card rank(s) and suite(s)
+    def initialize (rank, suit)
+        @rank = rank
+        @suit = suit
+        @value = calculate_value
+    end
 
-	def calculate_value
-		if ['J', 'Q', 'K'].include?(@rank)
-			10
-		elsif @rank == 'A'
-			11
-		else
-			@rank.to_i
-		end
-	end
+    def calculate_value
+        if ['J', 'Q', 'K'].include?(@rank)
+            10
+        elsif @rank == 'A'
+            11
+        else
+            @rank.to_i
+        end
+    end
 end # end Card
 
 # Deck class
 class Deck
-	attr_reader :cards
-	
-	def initialize
-		@cards = []
-		# Generate the deck of cards
-		%w(Hearts Diamonds Clubs Spades).each do |suit|
-			%w(2 3 4 5 6 7 8 9 10 J Q K A).each do |rank|
-			card = Card.new(rank, suit)
-			@cards << card
-			end
-		end
-		shuffle_cards
-	end
-	
-	def shuffle_cards
-		@cards.shuffle!
-	end
-	
-	def draw
-		@cards.pop || Card.new("", "")  # Ensure draw returns a valid card object
-	  end
+    attr_reader :cards
+    
+    def initialize
+        @cards = []
+        # Generate the deck of cards
+        %w(Hearts Diamonds Clubs Spades).each do |suit|
+            %w(2 3 4 5 6 7 8 9 10 J Q K A).each do |rank|
+            card = Card.new(rank, suit)
+            @cards << card
+            end
+        end
+        shuffle_cards
+    end
+    
+    def shuffle_cards
+        @cards.shuffle!
+    end
+    
+    def draw
+        @cards.pop || Card.new("", "")  # Ensure draw returns a valid card object
+      end
 end # end Deck
-	
+    
 # User class
 class User
-	attr_reader :player_hand, :name
+    attr_reader :player_hand, :name
   
-	def initialize(name)
-	  @name = name
-	  @player_hand = [] # Initialize player_hand as an empty array
-	end
+    def initialize(name)
+      @name = name
+      @player_hand = [] # Initialize player_hand as an empty array
+    end
   
-	def start_game
-	  # Implement if needed
-	end
+    def start_game
+      # Implement if needed
+    end
   
-	def total 
-		total_value = 0
-		aces_count = 0
+    def total 
+        total_value = 0
+        aces_count = 0
 
-	  @player_hand.each do |card|
-		total_value += card.value
-		aces_count += 1 if card.rank == 'A'
-	  end
+      @player_hand.each do |card|
+        total_value += card.value
+        aces_count += 1 if card.rank == 'A'
+      end
 
-	  # adjust value if there are aces
-	  while total_value > 21 && aces_count > 0
-		total_value -= 10
-		aces_count -= 1
-	  end
+      # adjust value if there are aces
+      while total_value > 21 && aces_count > 0
+        total_value -= 10
+        aces_count -= 1
+      end
 
-	  total_value
-	end
+      total_value
+    end
   
-	def add_card(card)
-	  @player_hand << card
-	end
+    def add_card(card)
+      @player_hand << card
+    end
   end # end User  
 
 # Dealer class
 class Dealer
-	attr_reader :dealer_hand
-	def initialize
-		@dealer_hand = []
-	end
+    attr_reader :dealer_hand
+    def initialize
+        @dealer_hand = []
+    end
 
-	def add_card(card)
-		@dealer_hand << card
-	end
+    def add_card(card)
+        @dealer_hand << card
+    end
 
-	def total 
-		total_value = 0
-		aces_count = 0
+    def total 
+        total_value = 0
+        aces_count = 0
 
-	  @dealer_hand.each do |card|
-		total_value += card.value
-		aces_count += 1 if card.rank == 'A'
-	  end
+      @dealer_hand.each do |card|
+        total_value += card.value
+        aces_count += 1 if card.rank == 'A'
+      end
 
-	  # adjust value if there are aces
-	  while total_value > 21 && aces_count > 0
-		total_value -= 10
-		aces_count -= 1
-	  end
+      # adjust value if there are aces
+      while total_value > 21 && aces_count > 0
+        total_value -= 10
+        aces_count -= 1
+      end
 
-	  total_value
-	end
+      total_value
+    end
 end # end Dealer
 
 
 # Initialize and deal cards for the game
-game = Game.new("Player", text_player_card1, text_player_card2, text_dealer_card1, text_player_total, text_dealer_total)
+game = Game.new("Player", text_player_card1, text_player_card2, text_dealer_card1, text_dealer_card2, text_player_total, text_dealer_total)
 
 # Event listeners for keyboard input
 on :key_down do |event|
-	if event.key == "d" || event.key == "D"  # Deal cards when 'd' or 'D' is pressed
-	  game.deal_cards_initially
-	elsif event.key == "h" || event.key == "H"  # Hit when 'h' or 'H' is pressed
-	  game.hit
-	elsif event.key == "s" || event.key == "S"  # Stand when 's' or 'S' is pressed
-	  game.stand
-	end
+    if event.key == "d" || event.key == "D"  # Deal cards when 'd' or 'D' is pressed
+      game.deal_cards_initially
+    elsif event.key == "h" || event.key == "H"  # Hit when 'h' or 'H' is pressed
+      game.hit
+    elsif event.key == "s" || event.key == "S"  # Stand when 's' or 'S' is pressed
+      game.stand
+    end
   end
   
 # Show the window
